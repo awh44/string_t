93927cac4645e637d164faa1c61d8c5a84e62de9
Austin Herring <awh44@drexel.edu>
2015-10-18 18:12:53 -0400

    Added functionality needed and fixed bugs found when developing an FTP client

--- string_t.c
+++ string_t.c
@@ -11,6 +11,11 @@ void string_uninitialize(string_t *s)
 	char_vector_uninitialize(s);
 }
 
+size_t string_length(string_t *s)
+{
+	return char_vector_size(s);
+}
+
 void string_assign_from_char_array(string_t *s, char *arr)
 {
 	string_assign_from_char_array_with_size(s, arr, strlen(arr));
@@ -38,7 +43,19 @@ void string_concatenate_char_array(string_t *s, char *arr)
 	}
 }
 
+void string_concatenate_char_array_with_size(string_t *s, char *arr, size_t n)
+{
+	char_vector_size_at_least(s, s->elements + n);
+	memcpy(s->array + s->elements, arr, n);
+	s->elements += n;
+}
+
 string_t *string_split(string_t *s, char delim, size_t *num)
+{
+	return string_split_skip_consecutive(s, delim, num, 0);
+}
+
+string_t *string_split_skip_consecutive(string_t *s, char delim, size_t *num, uint8_t skip)
 {
 	string_t *ret_val = NULL;
 	*num = 0;
@@ -56,6 +73,14 @@ string_t *string_split(string_t *s, char delim, size_t *num)
 			string_assign_from_char_array(ret_val + *num - 1, start_pos);
 			
 			s->array[i] = delim;
+
+			//skip any consecutive delimters, if desired
+			while (s->array[i + 1] == delim && skip)
+			{
+				i++;
+			}
+
+			//update the start position
 			start_pos  = s->array + i + 1;
 		}
 	}
@@ -111,5 +136,37 @@ void string_getline(string_t *s, FILE *stream)
 	{
 		char_vector_push_back(s, next_char);
 		next_char = fgetc(stream);
+	}
+}
+
+int string_compare_char_array(string_t *s1, char *s2)
+{
+	return strcmp(string_c_str(s1), s2);
+}
+
+void string_trim(string_t *s)
+{
+	while (char_vector_get(s, 0) == ' ')
+	{
+		char_vector_remove(s, 0);
+	}
+
+	size_t last = string_length(s);
+	while (char_vector_get(s, last - 1) == ' ')
+	{
+		char_vector_pop_back(s);
+		last = string_length(s);
+	}
+}
+
+void string_replace(string_t *s, char oldc, char newc)
+{
+	size_t i;
+	for (i = 0; i < s->elements; i++)
+	{
+		if (s->array[i] == oldc)
+		{
+			s->array[i] = newc;
+		}
 	}
 }
